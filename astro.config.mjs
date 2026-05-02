8f8a1469ab5a6a95bd5d20cc7af15dfebf043b9a
sanasuno <sanasunozzz@gmail.com>
2026-05-02 21:30:12 +0900

    環境変数の設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+const SITE_URL = process.env.SITE_URL ||'https://example.github.io';
+const BASE_PATH = process.env.BASE_PATH ||'/';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), sitemap()],
+  site: SITE_URL,
+  base: BASE_PATH
+});
