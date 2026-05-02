d88a351992e580940f717785fde19231047d7e7c
sanasuno <sanasunozzz@gmail.com>
2026-05-02 21:45:38 +0900

    環境変数

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-const SITE_URL = process.env.SITE_URL ||'https://example.github.io';
-const BASE_PATH = process.env.BASE_PATH ||'/';
-
+const SITE_URL = process.env.SITE_URL || 'https://example.github.io';
+const BASE_PATH = process.env.BASE_PATH || '/';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
-  site: SITE_URL,
-  base: BASE_PATH
+    site: SITE_URL,
+    base: BASE_PATH,
+    integrations: [mdx(), sitemap()]
 });
